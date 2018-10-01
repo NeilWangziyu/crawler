1d24a914e9e7d5055d6cc497582c6f00188543a2
Ziyu <16078718+NeilWangziyu@users.noreply.github.com>
2018-10-01 22:40:58 +0800

    2018-10-01

---
+++ Hupu/JD_ejiao2.py
@@ -0,0 +1,65 @@
+import requests
+from urllib.parse import urlencode
+import lxml
+from lxml import html
+from lxml import etree
+import re
+import xlwt
+
+itempage = 'https://item.jd.com/1411131635.html#comment'
+
+
+workbook = xlwt.Workbook()
+sheet = workbook.add_sheet("sheet1")
+style = xlwt.easyxf('font: bold 1')
+sheet.write(0, 0, itempage, style)
+sheet.write(1, 0, 'Title', style)
+
+count = 2
+
+
+base_URL = 'https://sclub.jd.com/comment/productPageComments.action?'
+
+
+
+for i in range (100):
+    params = {
+        'callback':'fetchJSON_comment98vv222',
+        'productId':'1411131635',
+        'score':'0',
+        'sortType':'5',
+        'page':'{}'.format(i),
+        'pageSize':'10',
+        'isShadowSku':'0',
+        'rid':'0',
+        'fold':'1'
+    }
+
+    URL = base_URL + urlencode(params)
+    try:
+        detailhtml = requests.get(URL, timeout=3).content
+    except:
+        print("fail to gain file, page:",i)
+        break
+
+    text = detailhtml.decode('GBK')
+
+    re_text = '"content":"\W+"'
+
+    result = re.findall(r'(\"content\"\:\")(\S+)(\"\,)', text)
+
+    for each in result:
+        sheet.write(count, 0, each)
+        count = count + 1
+
+    print("finished page",i)
+
+print("finished")
+workbook.save("JD1411131635.xls")
+
+
+# URL = base_URL + urlencode(params)
+# print(URL)
+# detailhtml = requests.get(URL, timeout=3).content
+# print(detailhtml.decode('GBK'))
+#
--- Hupu/test.py
+++ Hupu/test.py
@@ -1,39 +1,32 @@
 import requests
+from urllib.parse import urlencode
 import lxml
 from lxml import html
+from lxml import etree
+import re
+import xlwt
+import json
 
-URL = 'https://my.hupu.com/258051531319630'
-URL_2 = 'https://my.hupu.com/68154067684448'
-URL_3 = 'https://my.hupu.com/53347953706220'
-URL_4 = 'https://my.hupu.com/185941226680433'
-URL_5 = 'https://my.hupu.com/19415579077176'
-URL_6 = 'https://my.hupu.com/dennisdo'
+base_URL = 'https://sclub.jd.com/comment/productPageComments.action?'
 
-detailhtml = requests.get(URL_2, timeout=3)
-detailpc = detailhtml.text
-detailpc.encode('utf-8')
-detail_page = lxml.html.fromstring(detailpc)
+params = {
+        'callback':'fetchJSON_comment98vv222',
+        'productId':'1411131635',
+        'score':'0',
+        'sortType':'5',
+        'page':'1',
+        'pageSize':'10',
+        'isShadowSku':'0',
+        'rid':'0',
+        'fold':'1'
+    }
 
-# 性别，所在地，加入时间，NBA主队
-person = detail_page.xpath(".//span [@class='f666']/text()")
-print(len(person))
-print(person)
-# NBAsupport = detail_page.xpath("//*[@id='main']/div[1]/div[2]/div/span[6]/a/text()")
-# print(NBAsupport)
+URL = base_URL + urlencode(params)
 
-gender = detail_page.xpath(".//span [@itemprop='gender']/text()")
-print(gender)
+detailhtml = requests.get(URL, timeout=3).content
 
-address = detail_page.xpath(".//span [@itemprop='address']/text()")
-print(address)
+text = detailhtml.decode('GBK')
 
-people_list = detail_page.xpath('.//div[@id="following"]/ul')
-for each in people_list:
-    title_tem = each.xpath(".//a[@class='u']/@href")
-    print(title_tem)
+result = re.findall(r'(\"content\"\:\")(\S+)(\"\,)', text)
 
-people_list = detail_page.xpath('.//div[@id="visitor"]/ul')
-print(people_list)
-for each in people_list:
-    title_tem = each.xpath(".//a[@class='u']/@href")
-    print(title_tem)
+print(result)
